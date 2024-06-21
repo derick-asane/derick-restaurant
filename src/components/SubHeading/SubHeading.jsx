78050493bf4f1c77c0906074e0ed013f307a48a4
derick-asane <derickasane@gmail.com>
2024-06-21 18:29:56 +0100

    starting the restaurant landing page, the navbar and header implemented already

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,8 @@
+import { images } from "../../constants";
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="P__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon" className="spoon__img" />
+  </div>
+);
+export default SubHeading;
