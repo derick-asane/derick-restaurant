3366e0a6162c95d80e217fc20c138f4308fd77a9
derick-asane <derickasane@gmail.com>
2024-06-22 23:37:05 +0100

    implemented aboutus, specialmenu

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about__spoon" className="spoon__img" />
+        <p className="p__opensans">
+          The git add command adds a change in the working directory to the
+          staging area. It tells Git that you want to include updates to a
+          particular file in the next commit.
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about__spoon" className="spoon__img" />
+        <p className="p__opensans">
+          The git add command adds a change in the working directory to the
+          staging area. It tells Git that you want to include updates to a
+          particular file in the next commit.
+        </p>
+        <button type="button" className="custom__button">
+          Know more
+        </button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
-
-import './SpecialMenu.css';
+import React from "react";
+import { images, data } from "../../constants";
+import "./SpecialMenu.css";
+import { SubHeading, MenuItem } from "../../components";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specilaMenu-title">
+      <SubHeading title="Menu that feet your palate" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app__specialMenu-menu_items">
+          {data.wines.map((wine, index) => (
+            <MenuItem
+              key={wine.title}
+              title={wine.title}
+              tags={wine.tags}
+              price={wine.price}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="" />
+      </div>
+
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className="app__specialMenu-menu_items">
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem
+              key={cocktail.title}
+              title={cocktail.title}
+              tags={cocktail.tags}
+              price={cocktail.price}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+
+    <div className="" style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">
+        View More{" "}
+      </button>
+    </div>
   </div>
 );
 
