d0cd7582c89fa9c761d5c7471c126ca9eb15c729
derick-asane <derickasane@gmail.com>
2024-06-25 19:57:15 +0100

    implemented the intro, now working on the laurels, created a component AwardCard inside the laurels component

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,7 +1,7 @@
 import { images } from "../../constants";
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: "1rem" }}>
-    <p className="P__cormorant">{title}</p>
+    <p className="p__cormorant">{title}</p>
     <img src={images.spoon} alt="spoon" className="spoon__img" />
   </div>
 );
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import "./Intro.css";
+import { meal } from "../../constants";
+import { useRef, useState } from "react";
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
 
-import './Intro.css';
+  const handleVideo = () => {
+    setPlayVideo((prePlayVideo) => !prePlayVideo);
+    playVideo ? vidRef.current.pause() : vidRef.current.play();
+  };
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        ref={vidRef}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
