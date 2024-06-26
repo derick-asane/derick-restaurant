0819d41b00df0985ed3c77ba786ac05e195f74c5
derick-asane <derickasane@gmail.com>
2024-06-26 20:00:44 +0100

    Laurels , Gallery, Footer, FindUs, FooterOverlay, still working on the footer component. (6:19:05)

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-
-import './Footer.css';
-
+import "./Footer.css";
+import { FooterOverlay, Newsletter } from "../../components";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import { images } from "../../constants";
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans"></p>
+      </div>
+      <div className="app__footer-links_work"></div>
+    </div>
+    <div className="footer__copyright">
+      <p className="p__opensans">2021 Gerícht. All Rights reserved.</p>
+    </div>
   </div>
 );
 
--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -26,7 +26,9 @@ const Laurels = () => (
         ))}
       </div>
     </div>
-    <div className="app__wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels" />
+    </div>
   </div>
 );
 
